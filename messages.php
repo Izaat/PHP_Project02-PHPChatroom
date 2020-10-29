216e5c5f0f78304446eaacae5486b127c4f88511
Izaat Tamrin <khairizat@hotmail.com>
2020-10-30 07:34:12 +0800

    Update messages.php

--- messages.php
+++ messages.php
@@ -1,30 +1,37 @@
 <html>
 <head>
-<meta http-equiv="refresh" content="10">
+<meta http-equiv="refresh" content="2">
+
 </head>
 <body>
 <?php
+//page refresh rate every 2 seconds
+//Change port for C:\xampp\phpMyAdmin if not connected to SQL
 // this is where the message from the server will go
 
 //connect to db
-$link = mysqli_connect('localhost', 'root', '', 'foxtrot');
+$link = mysqli_connect('localhost', 'root', '', 'im');
 if (!$link)
 {	die('Could not connect: ' . mysqli_error()); }
 else
 { 
-//echo 'were good';
-
+echo 'were good</br>';
 }
 
+//connect to database
+//mysql_select_db(im)
 
-
+//query the database
 $query = "SELECT * FROM messages";
 
+//fetch the results / convert result to array
 if ($result = mysqli_query($link, $query)) {
+    
 	/*fetch associative array */
 	while ($row = mysqli_fetch_row($result)) 
 	{
-		echo $row['3'].': '.'Room:'.$row['1'].', Tag:'.$row['2'].'</br>';
+        //number is column and row as specified echo $row['username-columnid'].' says: '.$row['message-columnid'].'</br>';
+		echo $row['3'].' says: '.$row['1'].'</br>';
 	}
 	mysqli_free_result($result);
 }
